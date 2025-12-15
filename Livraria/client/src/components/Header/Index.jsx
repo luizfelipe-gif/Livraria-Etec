04f1adbd794c6622c808c797969d444b3a737dfc
luizfelipe-gif <luizf.892002@gmail.com>
2025-12-15 18:55:07 -0300

    v1.2.7 -> Implementação da lógica pra aparecer imagem na Header, além da troca do navigate para Link pra fins de acessibilidade.

--- Livraria/client/src/components/Header/Index.jsx
+++ Livraria/client/src/components/Header/Index.jsx
@@ -1,67 +1,116 @@
 import './Header.css';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { getUser } from '../../helpers/auth'
 import api from '../../services/api';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getUser } from '../../helpers/auth'
 
 function Header() {
    const navigate = useNavigate();
-   const [usuario, setUsuario] = useState(null);
 
-   useEffect(() => { // Revisar essa função pra obter os dados do usuário logado
+   const [usuario, setUsuario] = useState(null);
+   const [profilePhoto, setProfilePhoto] = useState(null);
+
+   const [dados, setDados] = useState({
+      userId: '',
+      name: '',
+      email: '',
+      createdAt: ''
+   });
+
+   useEffect(() => {
+      const response = getUser();
+      setDados({userId: response.userId, name: response.user, email: response.email, createdAt: response.createdAt});
+   }, []);
+
+   useEffect(() => {
       function getUserData() {
          const usuario = getUser();
          setUsuario(usuario);
-   
       };
       getUserData();
    }, []);
 
+
    function handleLogout() {
       sessionStorage.removeItem('token');
       navigate('/login');
-   }
+   };
 
-   function homeNavigate() {
-      if (!usuario) return handleLogout();
-      navigate(`${usuario.typeUser}/home`);
-   }
+   // function homeNavigate() {
+   //    if (!usuario) return handleLogout();
+   //    navigate(`${usuario.typeUser}/home`);
+   // };
+
+
+   useEffect(() => {
+      async function getUserProfile() {
+         try {
+            const token = sessionStorage.getItem('token');
+            const response = await api.get(`/user/${dados?.userId}`, {
+               headers: {Authorization: `Bearer ${token}`}
+            });
+
+            console.log("resposta", response);
+
+            const imagemURL = response?.data?.response?.url_photo_profile;
+
+            if (imagemURL) {
+               setProfilePhoto(imagemURL);
+            };
+         }
+         catch (error) {
+            console.log(error);
+            toast.error(error, {
+               position: "top-right",
+               autoClose: 3000,
+               hideProgressBar: false,
+               closeOnClick: true,
+               pauseOnHover: true,
+               draggable: true,
+               progress: undefined,
+               theme: "light",
+            });
+         };
+      };
+      getUserProfile();
+   }, [dados.userId]);
+
    
    return (
       <header className="header">
-         <div className="navbar-titulo categoria"  onClick={() => navigate("/home")}>
+         <Link to="/home" className="header-titulo">
             <img src="client/public/logo.svg"></img>
             <span>WikiLivros</span>
-         </div>
+         </Link>
 
-         <div className="navbar-categorias">
-            <div onClick={() => navigate("/author")}>
+         <div className="header-categorias" href="asfs">
+            <Link to="/author">
                <img src="client/public/writer.svg"></img>
                <p>Autores</p>
-            </div>
+            </Link>
 
             <div className='divisoria'></div>
 
-            <div onClick={() => navigate("/publishers")}>
+            <Link to="/publishers">
                <img src="client/public/company.svg"></img>
                <p>Editoras</p>
-            </div>
+            </Link>
 
             <div className='divisoria'></div>
 
-            <div onClick={() => navigate("/list-books")}>
+            <Link to="/list-books">
                <img src="client/public/book.svg"></img>
                <p>Livros</p>
-            </div>
+            </Link>
          </div>
 
-         <div className="navbar-usuario">
-            <div onClick={() => navigate("/profile")}>
-               <img src="client/public/user_profile.svg"></img>
+         <div className="header-usuario">
+            <Link to="/profile" className='userImage'>
+               <img src={profilePhoto || '/client/public/user_profile.svg'} alt='Foto de Perfil' className="profile-photo"/>
                <p className='usuario'>{usuario?.user}</p>
-            </div>
+            </Link>
 
-            <div onClick={() => {sessionStorage.removeItem('token') || navigate("/login")}}>
+            <div onClick={() => handleLogout()}>
                <img src="client/public/logout.svg"></img>
                <p>Sair</p>
             </div>
